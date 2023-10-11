f18e4df800aa5ff1996493c602f5257c48c647f7
irisagfe <irisagfe@gmail.com>
2023-10-11 16:42:50 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,10 +1,6 @@
 let express = require('express');
 let app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
-});
-
 app.get("/json", (req, res) => {
   res.json({ message : "Hello json" });
 });
