07b92c9ab09f16f970f8f792f16b0d58bd630e89
irisagfe <irisagfe@gmail.com>
2023-10-12 09:22:25 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,8 +4,8 @@ let app = express();
 app.get("/json", (req, res) => {
   res.json({ message : "Hello json" });
 });
-#app.use("/public", express.static(__dirname + "/public"))
-#console.log("Hello World");
+//app.use("/public", express.static(__dirname + "/public"))
+//console.log("Hello World");
 
 
 
