3977a54c6a62c448358baf3c79ff982dd73d58a3
irisagfe <irisagfe@gmail.com>
2023-10-12 12:53:12 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 
-app.use(bodyParser.urlencoder({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use("/", (req, res, next) => {
   console.log(req.method + " " + req.path + " - " + req.ip)
   next()
